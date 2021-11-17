b7a06c3226bc12358a9ddc51eb71683f5903aebc
Luis Felipe Marconi <lmarconi@ciandt.com>
2021-11-17 12:25:38 -0300

    test: add scenario to function sum with another null param

--- calculator/test/main.spec.js
+++ calculator/test/main.spec.js
@@ -8,6 +8,10 @@ test("1 + 2 must return 3", () => {
   expect(sum(1,2)).toBe(3);
 })
 
-test("returns string 'parameter must not be null'", () => {
+test("returns string 'parameter must not be null' when first parameter is null", () => {
+  expect(sum(null, 1)).toBe("parameter must not be null");
+})
+
+test("returns string 'parameter must not be null' when second parameter is null", () => {
   expect(sum(1,null)).toBe("parameter must not be null");
 })
