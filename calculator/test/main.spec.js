9d97dcd307e552d5158f8fa6a1c6aba4772171b2
Pedro Augusto Soares Pereira <pedrosoares@ciandt.com>
2021-11-17 11:55:36 -0300

    test: Created new scenario sum test

--- calculator/test/main.spec.js
+++ calculator/test/main.spec.js
@@ -7,5 +7,9 @@
 const sum = require('../src/main');
 
 test("1 + 1 must return 2", () => {
-  expect(sum()).toBe(2);
+  expect(sum(1,1)).toBe(2);
+})
+
+test("1 + 2 must return 3", () => {
+  expect(sum(1,2)).toBe(3);
 })
