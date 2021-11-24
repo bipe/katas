6ec946e6d00fda6f6f2a0b2ac8b236639aa806c3
Fabio Martins Silva <CIANDT\fmsilva@lnb029562spo.ciandt.global>
2021-11-24 12:36:49 -0300

    test: add scenario to validate params when call function mult

--- calculator/test/multiplication.spec.js
+++ calculator/test/multiplication.spec.js
@@ -1,19 +1,38 @@
+const mult = require('../src/multiplication');
 
 test("8 * 2 shoul return 16", () => {
-    num1 = 8;
-    num2 = 2;
+    const num1 = 8;
+    const num2 = 2;
 
-    result = multi(num1, num2);
+    const result = mult(num1, num2);
 
     expect(result).toBe(16);
 });
 
 test("5 * 6 should return 30", () => {
-    num1 = 5;
-    num2 = 6;
+    const num1 = 5;
+    const num2 = 6;
 
-    result = multi(num1, num2);
+    const result = mult(num1, num2);
 
     expect(result).toBe(30);
+});
+
+test("returns string 'parameter must not be null' when first parameter is null", () => {
+    const num1 = null;
+    const num2 = 1;
+
+    const result = mult(num1, num2)
+
+    expect(result).toBe("parameter must not be null");
+});
+
+test("returns string 'parameter must not be null' when second parameter is null", () => {
+    const num1 = 1;
+    const num2 = null;
+
+    const result = mult(num1, num2)
+
+    expect(result).toBe("parameter must not be null");
 });
 
