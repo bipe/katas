609893b083e5bed1ed62ea230f07769bfa1bb2bd
Luis Felipe Marconi <lmarconi@ciandt.com>
2021-11-24 12:14:03 -0300

    test: add null parameter subtraction tests

--- calculator/test/subtraction.spec.js
+++ calculator/test/subtraction.spec.js
@@ -1,19 +1,37 @@
 const sub = require('../src/subtraction');
 
 test("5 - 7 should return -2", () => {
-    num1 = 5;
-    num2 = 7;
+    const num1 = 5;
+    const num2 = 7;
 
-    result = sub(num1, num2);
+    const result = sub(num1, num2);
 
     expect(result).toBe(-2);
 });
 
 test("4 - 1 should return 3", () => {
-    num1 = 4;
-    num2 = 1;
+    const num1 = 4;
+    const num2 = 1;
 
-    result = sub(num1,num2);
+    const result = sub(num1,num2);
 
     expect(result).toBe(3);
 });
+
+test("returns string 'parameter must not be null' when first parameter is null", () => {
+    const num1 = null;
+    const num2 = 1;
+
+    const result = sub(num1, num2)
+
+    expect(result).toBe("parameter must not be null");
+  })
+
+  test("returns string 'parameter must not be null' when second parameter is null", () => {
+    const num1 = 1;
+    const num2 = null;
+
+    const result = sub(num1, num2)
+
+    expect(result).toBe("parameter must not be null");
+  })
