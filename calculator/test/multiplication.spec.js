b859bad75cf63cb22a0cd74918c14388f593ac73
CIANDT\allinny <allinny@ciandt.com>
2021-11-24 12:26:17 -0300

    test: add scenario for multiplication function

---
+++ calculator/test/multiplication.spec.js
@@ -0,0 +1,19 @@
+
+test("8 * 2 shoul return 16", () => {
+    num1 = 8;
+    num2 = 2;
+
+    result = multi(num1, num2);
+
+    expect(result).toBe(16);
+});
+
+test("5 * 6 should return 30", () => {
+    num1 = 5;
+    num2 = 6;
+
+    result = multi(num1, num2);
+
+    expect(result).toBe(30);
+});
+
