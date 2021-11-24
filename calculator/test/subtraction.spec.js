eff5bf036cbe889c3e89cf07f87ec064d2cf4c2d
Luis Felipe Marconi <lmarconi@ciandt.com>
2021-11-24 12:10:52 -0300

    refactor: refactor subtraction tests

--- calculator/test/subtraction.spec.js
+++ calculator/test/subtraction.spec.js
@@ -1,10 +1,15 @@
 const sub = require('../src/subtraction');
 
-test("5 - 7 should return 2", () => {
-    expect(sub(5, 7)).toBe(-2);
+test("5 - 7 should return -2", () => {
+    num1 = 5;
+    num2 = 7;
+
+    result = sub(num1, num2);
+
+    expect(result).toBe(-2);
 });
 
-test("4 - 1 should return 2", () => {
+test("4 - 1 should return 3", () => {
     num1 = 4;
     num2 = 1;
 
