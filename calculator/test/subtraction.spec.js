b1437aef2f13973d2a2fad96bb6315ef8b5c0999
Fabio Martins Silva <CIANDT\fmsilva@lnb029562spo.ciandt.global>
2021-11-24 12:04:30 -0300

    test: add test to explore another scenarios for subtraction

--- calculator/test/subtraction.spec.js
+++ calculator/test/subtraction.spec.js
@@ -1,5 +1,14 @@
 const sub = require('../src/subtraction');
 
 test("5 - 7 should return 2", () => {
-    expect(sub(5, 7)).toBe(2);
+    expect(sub(5, 7)).toBe(-2);
+});
+
+test("4 - 1 should return 2", () => {
+    num1 = 4;
+    num2 = 1;
+
+    result = sub(num1,num2);
+
+    expect(result).toBe(3);
 });
